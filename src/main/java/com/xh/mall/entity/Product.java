842f43026190936ad6fd533fb1d04ac0c9106080
hotWolf <54540046+LBBZ@users.noreply.github.com>
2025-02-23 20:53:09 +0800

    商品

---
+++ src/main/java/com/xh/mall/entity/Product.java
@@ -0,0 +1,19 @@
+package com.xh.mall.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Integer stock;
+    private String category;
+    private String description;
+}

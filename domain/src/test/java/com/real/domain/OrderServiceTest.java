aa9f40b4c6df137ccb366fd646c4176df3c350c7
hotWolf <54540046+LBBZ@users.noreply.github.com>
2025-02-28 13:55:08 +0800

    修复bug

--- domain/src/test/java/com/real/domain/OrderServiceTest.java
+++ domain/src/test/java/com/real/domain/OrderServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 import java.util.Arrays;

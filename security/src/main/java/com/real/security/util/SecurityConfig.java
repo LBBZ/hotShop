ba344c693cb7c31150c2639f60b9da2cd4820bd2
hotWolf <54540046+LBBZ@users.noreply.github.com>
2025-03-11 15:31:13 +0800

    微调

--- security/src/main/java/com/real/security/util/SecurityConfig.java
+++ security/src/main/java/com/real/security/util/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers("/portal/auth/logout",
                                 "/portal/auth/refresh").authenticated()
                         .requestMatchers("/portal/auth/**",
+                                "/admin/auth/**",
                                 "/portal/products/**").permitAll()
                         .anyRequest().authenticated()
                 )

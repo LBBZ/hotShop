19c90d5db69720034555b401b6cde32e36570684
hotWolf <54540046+LBBZ@users.noreply.github.com>
2025-03-16 21:09:33 +0800

    完成工厂创建类

--- domain/src/main/java/com/real/domain/redisService/RedisTemplateGenerator.java
+++ domain/src/main/java/com/real/domain/redisService/RedisTemplateGenerator.java
@@ -2,9 +2,11 @@ package com.real.domain.redisService;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisTemplateGenerator {
@@ -14,18 +16,50 @@ public class RedisTemplateGenerator {
     private int redisPort;
     @Value("${spring.redis.password}")
     private String redisPassword;
-
+    // 存储redis数据库链接
     private final RedisTemplate<?, ?>[] redisTemplate = new RedisTemplate[16];
 
-    private final RedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+    /**
+     * 用于根据配置建立工厂
+     * @param dateBase 数据库编号
+     */
+    private LettuceConnectionFactory redisLettuceConnectionFactory(int dateBase) {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        configuration.setPassword(redisPassword);
+        configuration.setDatabase(dateBase);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(configuration);
+        lettuceConnectionFactory.afterPropertiesSet();
+        return lettuceConnectionFactory;
+    }
+
+    /**
+     * 用于根据配置建立RedisTemplate
+     * @param dateBase 数据库编号
+     * @return 返回建立的RedisTemplate
+     */
+    private RedisTemplate<?, ?> generateRedisTemplate(int dateBase) {
+        LettuceConnectionFactory lettuceConnectionFactory = redisLettuceConnectionFactory(dateBase);
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        // 配置序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
 
     public RedisTemplate<?, ?> getRedisTemplate(int dataBase) {
         if (dataBase < 0 || dataBase >= 16) {
             return null;
         }
         if (redisTemplate[dataBase] == null) {
-            redisTemplate[dataBase] = new RedisTemplate<>();
-
+            redisTemplate[dataBase] = generateRedisTemplate(dataBase);
         }
         return redisTemplate[dataBase];
     }

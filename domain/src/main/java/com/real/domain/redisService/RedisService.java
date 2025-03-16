910ce2b74c36ccbeddfd3ea0afd414f8ebdd09ec
hotWolf <54540046+LBBZ@users.noreply.github.com>
2025-03-16 21:27:08 +0800

    微调

--- domain/src/main/java/com/real/domain/redisService/RedisService.java
+++ domain/src/main/java/com/real/domain/redisService/RedisService.java
@@ -1,7 +1,6 @@
 package com.real.domain.redisService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
